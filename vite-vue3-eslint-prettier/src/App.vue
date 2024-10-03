b6799913556e6181c528421eb329318fb35fb150
Sebastian Fortuin <sebastian@rlabs.org>
2024-10-03 10:52:46 +0200

    CSS and Vite

--- vite-vue3-eslint-prettier/src/App.vue
+++ vite-vue3-eslint-prettier/src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+// import './assets/main.css';
+
+
+
+
+
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
@@ -10,6 +15,9 @@ import HelloWorld from "./components/HelloWorld.vue";
 </template>
 
 <style>
+
+/*@import './assets/main.css' (max-width: 800px);*/
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vite-vue3-eslint-prettier/src/main.js
+++ vite-vue3-eslint-prettier/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+// import styles
+import './assets/main.css';
+
+
 createApp(App).mount("#app");
